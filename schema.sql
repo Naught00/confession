64f281138b0e66b94dde3f379cb62f5f0bdc0022
Mark Naughton <mark@marknaughton.com>
2023-03-13 16:57:44 +0000

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,30 +1,25 @@
 CREATE TABLE posts (
-	id INTEGER PRIMARY KEY,
-	title TEXT NOT NULL,
-	text TEXT NOT NULL,
-	pic TEXT, 
-	poll_id integer
-);
-
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        text TEXT NOT NULL,
+        pic TEXT
+, timestamp integer, poll_id integer);
 CREATE TABLE replies (
-	id INTEGER PRIMARY KEY,
-	post_id INTEGER,
-	reply TEXT 
-);
-
-
+        id INTEGER PRIMARY KEY,
+        post_id INTEGER,
+        reply TEXT
+, timestamp integer);
 CREATE TABLE replies_to_replies (
-	id INTEGER PRIMARY KEY,
-	post_id INTEGER,
-	reply_id INTEGER,
-	reply TEXT 
-);
-
+        id INTEGER PRIMARY KEY,
+        post_id INTEGER,
+        reply_id INTEGER,
+        reply TEXT
+, timestamp integer);
 CREATE TABLE polls (
-	id INTEGER PRIMARY KEY,
-	title text,
-	option1 text,
-	option2 text,
-	option1p integer,
-	option2p integer
+        id INTEGER PRIMARY KEY,
+        title text,
+        option1 text,
+        option2 text,
+        option1p integer,
+        option2p integer
 );
